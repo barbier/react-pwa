29c3bfcb0114a8c7d64ed61f8420a7f33811414d
Barbier <gabrielnasb@gmail.com>
2018-09-27 16:27:44 -0300

    Chapter 03 - Login Component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App'
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 if (module.hot) {
-    module.hot.accept('./App', () => {
-        const nextApp = require('./App').default
-        ReactDOM.render(
-            <App />,
-            document.getElementById('root')
-        )
-    })
+  module.hot.accept('./components/App', () => {
+    const NextApp = require('./components/App').default;
+    ReactDOM.render(
+     <App/>,
+     document.getElementById('root')
+    );
+  });
 }
